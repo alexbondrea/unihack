5e5d101284e14c0a5e60f3fa341fd7a6ba6a7240
Darius <77147557+SDarius08@users.noreply.github.com>
2021-12-04 19:16:36 +0200

    Create info.h

---
+++ info.h
@@ -0,0 +1,151 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+void generalEVP_info() {
+    cout<<endl;
+
+    cout<<"Program de lucru pentru relatii publice:\n";
+    cout<<left;
+    cout<<
+        setw(20) << "[Program]" << setw(20) << "[Depunerea actelor]" << setw(20) << "[Eliberarea documentelor]" <<
+        "\n" <<
+        setw(20) << "Luni - Vineri" << setw(20) << "   08.00 - 19.00" << setw(20) << "     19.00 - 20.00";
+
+    cout<<endl<<endl;
+
+    cout<<"GDPR: https://mag.wcoomd.org/uploads/2018/05/blank.pdf \n\n";
+
+    cout<<"Director executiv: Ionel Popescu\n";
+    cout<<"Adresa: Oras, Strada\n";
+    cout<<"Numar de telefon: 0744/777777\n";
+    cout<<"FAX: 0261/777777\n";
+}
+
+void EVPID_info() {
+    cout<<endl;
+
+    cout<<"La nivelul Directiei de Evidenta a Persoanelor a Municipiului, cartile de identitate, cartile de identitate provizorii si vizele de resedinta se elibereaza, in termen de 7 zile calendaristice de la data depunerii cererii, cu exceptia situatiilor in care se efectueaza verificari in teren sau la alte structuri\n\n";
+
+    cout<<"Informatii generale: https://mag.wcoomd.org/uploads/2018/05/blank.pdf\n";
+
+    cout<<"\n"<<left;
+    cout<<"Termenul de valabilitate al C.I:\n";
+    cout<<
+        setw(20) << "[Varsta]" << setw(20) << "[Durata]" <<
+        "\n" <<
+        setw(20) << "14 - 18 ani" << setw(20) << "4 ani" <<
+        "\n" <<
+        setw(20) << "18 - 25 ani" << setw(20) << "7 ani" <<
+        "\n" <<
+        setw(20) << "Dupa 25 ani" << setw(20) << "10 ani" <<
+        "\n" <<
+        setw(20) << "Dupa 55 ani" << setw(20) << "Permanent";
+
+    cout<<endl<<endl;
+    cout<<"Eliberarea buletin la 14 ani: https://mag.wcoomd.org/uploads/2018/05/blank.pdf\n";
+
+    cout<<"Eliberarea buletin la 18 ani: https://mag.wcoomd.org/uploads/2018/05/blank.pdf\n";
+
+    cout<<"\n\n"<<left;
+    cout<<
+        setw(30) << "[Taxe speciale]" << setw(30) << "[Pret]" <<
+        "\n" <<
+        setw(30) << "Emiterea C.I." << setw(30) << "100 lei" <<
+        "\n" <<
+        setw(30) << "Emiterea C.I. provizoriu" << setw(30) << " 50 lei" <<
+        "\n" <<
+        setw(30) << "Acordarea vizei de resedinta" << setw(30) << " 50 lei" <<
+        "\n" <<
+        setw(30) << "Emiterea de dovezi" << setw(30) << " 20 lei";
+
+    cout<<"\n\n";
+
+    cout<<"Locatie: Oras, Strada\n";
+}
+
+void Truckauth_info() {
+    cout<<"\nInformatii generale:\nhttps://mag.wcoomd.org/uploads/2018/05/blank.pdf \n";
+    //general info
+
+    cout<<"\nTaxe:\nhttps://mag.wcoomd.org/uploads/2018/05/blank.pdf \n";
+    //taxes
+
+    cout<<"\nInformatii de contact: \nEmail: tir@primarie.ro\nNumar de telefon: 0748/777777\n";
+    //contact info
+
+}
+
+void EVPMarry_info() {
+    cout<<endl;
+    cout<<"Regulament: https://mag.wcoomd.org/uploads/2018/05/blank.pdf\n";
+
+    cout<<endl<<left;
+    cout<<
+        setw(20) << "   [Zi/Ora]" << setw(20) << "9:00 - 11:30" << setw(20) << "14:00 - 14:45" << setw(20) << "12:00 - 13:30" <<
+        "\n" <<
+        setw(20) << "Luni - Vineri" << setw(20) << "Nu sunt taxe " << setw(20) << " Nu sunt taxe" << setw(20) << "Taxa: 300 lei" <<
+        "\n" <<
+        setw(20) << "   Sambata" << setw(20) << "   50 lei" << setw(20) << "   200 lei" << setw(20) << "    111 lei" <<
+        "\n" <<
+        setw(20) << "   Duminica" << setw(20) << "  100 lei" << setw(20) << "   400 lei" << setw(20) << "    222 lei";
+
+    cout<<"\n\n[*]Precizam faptul ca taxele speciale se fac venit la bugetul local, ofiterii de stare civila care oficiaza casatoriile nu sunt si nu au fost retribuiti cu vreun procent din cuantumul acestor taxe.\n";
+    cout<<"[*]In situatia in care nici unul dintre viitorii soti nu au domiciliu sau resedinta in municipiu se va adauga o taxa suplimentara de 200 lei\n";
+    cout<<endl<<endl;
+
+    cout<<"Locatie: Primaria orasului, Str. Primariei\n";
+    cout<<"Informatii de contact:\nTelefon: 0745/777777\nEmail: starecivila@primarie.ro\n";
+}
+
+void ChildAllowance_info() {
+    cout<<endl;
+
+    cout<<"Informatii generale cu privinta la alocatia copiilor:\n"
+          "[*] Toti copiii in varsta de pana la 18 ani, cetateni romani, fara discriminare\n"
+          "[*] Tinerii care au implinit varsta de 18 ani si care urmeaza cursurile invatamantului liceal sau profesional, organizate in conditiile legii, pana la terminarea acestora.\n\n";
+
+    cout<<"Documente doveditoare pentru solicitarea alocatiei de stat pentru copii:\n"
+          "[*] Originalul si copia certificatului de nastere al copilului\n"
+          "[*] Originalul si copia actului de identitate al reprezentantului legal\n"
+          "[*] Documentele din care sa rezulte situatia juridica a copilului fata de reprezentantul legal\n"
+          "[*] Pentru copilul cu handicap, cererea este insotita si de copia certificatului de incadrare a copilului intr-un grad de handicap\n\n";
+
+    cout<<"Cerere pentru acordarea alocatiei de stat pentru copii: https://mag.wcoomd.org/uploads/2018/05/blank.pdf";
+}
+
+void ChildBirth_info() {
+    cout<<endl;
+
+    cout<<"Termenul pentru declararea si inregistrarea nasterii copilului este de:\n"
+          "[*] 30 zile de la data nastere, pentru copilul nascut viu si aflat in viata"
+          "[*] 3 zile de la data nastere, pentru copilul nascut mort"
+          "[*] 24 de ore de la data decesului pentru copilul nascut viu, dar care a decedat in termenului de 30 zile\n\n";
+
+    cout<<"Acte necesare pentru inregistrarea nasterii:\n"
+          "[*] Certificatul medical constatator al nasterii, intocmit pe formular tip, care trebuie sa poarte numar de inregistrare, data certa, sigiliul/stampila unitatii sanitare, semnatura si parafa medicului;\n"
+          "[*] Certificatul de casatorie (original si fotocopie) / sentinta definitiva / certificatul de divort (original si fotocopie)\n"
+          "[*] Actul de identitate al mamei (original si fotocopie)\n"
+          "[*] Actul de identitate al declarantului (original si fotocopie), dupa caz\n\n";
+
+    cout<<"Prenumele copilului:\n"
+          "In cazul in care prenumele copilului trecut in certificatul medical constatator al nasterii este diferit de cel declarat, inregistrarea nasterii se face in baza declaratiei scrise, semnata de ambii parinti, data in fata ofiterului de stare civila sau a notarului public, din care sa rezulte prenumele copilului.\n\n";
+
+    cout<<"Locatie: Spitalul Judetean (Maternitatea Municipiului)\n";
+}
+
+void LocalTaxes_info() {
+    //WIP
+}
+
+void ghiseulro_info() {
+    //WIP
+}
+
+void PublicRelations_info() {
+    //WIP
+}
+
+void DecisionalTransperacy_info() {
+    //WIP
+}
