5a862bfde52bfb7d7025a99c75d5753def302e66
Alexandru-Nadav Bondrea <47431790+alexbondrea@users.noreply.github.com>
2021-12-05 10:27:36 +0200

    Usage of continuous() for menu

--- source.cpp
+++ source.cpp
@@ -7,11 +7,13 @@ using namespace std;
 
 int main() {
     char input[100];
+    bool opt = 1;
     cout << "Bun venit in Primaria Digital! Aici gasesti informatiile necesare pentru a interactiona cu primaria noastra." << endl;
     cout << "Pentru a incepe, scrie ce vrei sa faci azi: ";
     std::vector<std::string> keywords;
+    while (opt==1) {
         cin.get(input, 100);
-        if (check_text(input, keywords={"buletin", "identitate", "ci", "c.i.", "pasaport"})==1) {
+        if (check_text(input, keywords={"buletin", "carte de identitate", "ci", "c.i.", "pasaport"})==1) {
             generalEVP_info();
             EVPID_info(); //check against C1: EVP ID
             continuous(opt);
@@ -19,13 +21,13 @@ int main() {
             generalEVP_info();
             EVPMarry_info(); //check against C2: EVP Marriage
             continuous(opt);
-        } else if (check_text(input, keywords={"tir", "camion", "autorizatie tir", "acces oras"})==1) {
+        } else if (check_text(input, keywords={"tir", "autorizatie tir", "acces oras"})==1) {
             Truckauth_info(); //check against C3: Truck permits
             continuous(opt);
         } else if (check_text(input, keywords={"alocatie", "bani copii"})==1) {
             ChildAllowance_info(); //check against C4: Legal guardian - child allowance
             continuous(opt);
-        }else if (check_text(input, keywords={"nastere", "certificat"})==1) {
+        }else if (check_text(input, keywords={"nastere", "certificat de nastere"})==1) {
             ChildBirth_info(); //check against C5: Registering the birth of a child
             continuous(opt);
         } else if (check_text(input, keywords={"taxe", "amenda", "amenzi", "taxa"})==1) {
@@ -35,9 +37,13 @@ int main() {
         } else if (check_text(input, keywords={"audienta", "primarie", "primaria", "primar", "primarul"})==1) {
             PublicRelations_info(); //check against C7: Public relations
             continuous(opt);
-        } else if (check_text(input, keywords={"decizii", "decizie", "hotarare"})==1) {
+        } else if (check_text(input, keywords={"decizii", "hotarare"})==1) {
             DecisionalTransperacy_info(); //check against C8: Decisional transperency
             continuous(opt);
+        } else {
+            cout << "Ne pare rau dar nu am inteles. Poti repeta?" << endl;
+            opt=1;
         }
+        cin.ignore(256, '\n');
     }
 }
