46f2413014ad53b8a6b95b19c198dc9adeb5e70c
Alexandru-Nadav Bondrea <47431790+alexbondrea@users.noreply.github.com>
2021-12-05 10:26:24 +0200

    Created continuous() function
    continuous() function is intended to help with the menu - working as a loop to allow users to make multiple inquiries in a single instance.

--- checker.h
+++ checker.h
@@ -19,3 +19,19 @@ int check_text(string input, vector<std::string> keywords) {
     }
     return 0;
 }
+
+void continuous(bool &opt){
+    char options[100];
+    std::vector<std::string> key;
+    cout << "Vrei sa cauti alte informatii? ";
+    cin >> options;
+    if (check_text(options, key={"da", "d", "yes", "ye", "aha", "sigur"})==1) {
+        opt=1;
+    } else if (check_text(options, key={"nu", "neh", "nup", "no", "altcandva", "nope"})==1) {
+        opt=0;
+        cout << "O zi buna!" << endl;
+    } else {
+        cout << "Poti repeta?" << endl;
+        continuous(opt);
+    }
+}
